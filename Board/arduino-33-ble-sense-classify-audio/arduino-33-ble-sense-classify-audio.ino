a0e0e54f8ad501db56fc199cb3edb824d10ff508
XindiSindy <386760499@qq.com>
2022-10-22 19:39:10 +0800

    Update arduino-33-ble-sense-classify-audio.ino

--- Board/arduino-33-ble-sense-classify-audio/arduino-33-ble-sense-classify-audio.ino
+++ Board/arduino-33-ble-sense-classify-audio/arduino-33-ble-sense-classify-audio.ino
@@ -5,6 +5,8 @@
 #include "tf_lite_model.h"       // TF Lite model file
 #include <eloquent_tinyml/tensorflow.h>
 
+#include <ArduinoBLE.h>
+
 // tune as per your needs
 #define SAMPLES 64
 #define GAIN (1.0f/50)
@@ -12,10 +14,16 @@
 #define SAMPLE_DELAY 20
 #define NUMBER_OF_LABELS   3     // number of voice labels
 const String words[NUMBER_OF_LABELS] = {"p", "a", "e"};  // words for each label
+const int sends[NUMBER_OF_LABELS] = {7, 8, 9};
 #define NUMBER_OF_INPUTS   SAMPLES
 #define TENSOR_ARENA_SIZE  4 * 1024
 #define NUMBER_OF_OUTPUTS  NUMBER_OF_LABELS
 Eloquent::TinyML::TensorFlow::TensorFlow<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> tf_model;
+
+BLEService ledService("180A"); // BLE LED Service
+
+// BLE LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("2A57", BLERead | BLEWrite);
 
 
 float features[SAMPLES];
@@ -28,6 +36,7 @@ Eloquent::ML::Port::SVM clf;
 void onAudio() {
     mic.update();
 }
+
 
 
 /**
@@ -56,20 +65,124 @@ void setup() {
     mic.begin();
     delay(3000);
     tf_model.begin(model_data);
+
+    while (!Serial);
+
+  // set LED's pin to output mode
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+  digitalWrite(LED_BUILTIN, LOW);         // when the central disconnects, turn off the LED
+  digitalWrite(LEDR, HIGH);               // will turn the LED off
+  digitalWrite(LEDG, HIGH);               // will turn the LED off
+  digitalWrite(LEDB, HIGH);                // will turn the LED off
+
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy failed!");
+
+    while (1);
+  }
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("Nano 33 BLE Sense");
+  BLE.setAdvertisedService(ledService);
+
+  // add the characteristic to the service
+  ledService.addCharacteristic(switchCharacteristic);
+
+  // add service
+  BLE.addService(ledService);
+
+  // set the initial value for the characteristic:
+  switchCharacteristic.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE LED Peripheral");
+    
 }
 
 
 void loop() {
+  // listen for Bluetooth® Low Energy peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+    digitalWrite(LED_BUILTIN, HIGH);            // turn on the LED to indicate the connection
+
+    // while the central is still connected to peripheral:
+    while (central.connected()) {
+      // if the remote device wrote to the characteristic,
+      // use the value to control the LED:
+      if (switchCharacteristic.written()) {
+        Serial.println(switchCharacteristic.value());
+        switch (switchCharacteristic.value()) {   // any value other than 0
+          case 49:
+            Serial.println("Red LED on");
+            digitalWrite(LEDR, LOW);            // will turn the LED on
+            digitalWrite(LEDG, HIGH);         // will turn the LED off
+            digitalWrite(LEDB, HIGH);         // will turn the LED off
+            break;
+          case 50:
+            Serial.println("Green LED on");
+            digitalWrite(LEDR, HIGH);         // will turn the LED off
+            digitalWrite(LEDG, LOW);        // will turn the LED on
+            digitalWrite(LEDB, HIGH);        // will turn the LED off
+            break;
+          case 51:
+            Serial.println("Blue LED on");
+            digitalWrite(LEDR, HIGH);         // will turn the LED off
+            digitalWrite(LEDG, HIGH);       // will turn the LED off
+            digitalWrite(LEDB, LOW);         // will turn the LED on
+            break;
+          default:
+            Serial.println(F("LEDs off"));
+            digitalWrite(LEDR, HIGH);          // will turn the LED off
+            digitalWrite(LEDG, HIGH);        // will turn the LED off
+            digitalWrite(LEDB, HIGH);         // will turn the LED off
+            break;
+        }
+      }
+    }
+
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+    digitalWrite(LED_BUILTIN, LOW);         // when the central disconnects, turn off the LED
+    digitalWrite(LEDR, HIGH);          // will turn the LED off
+    digitalWrite(LEDG, HIGH);        // will turn the LED off
+    digitalWrite(LEDB, HIGH);         // will turn the LED off
+  }
+
+
+
+
+
+
+
+
     if (recordAudioSample()) {
 //        Serial.print("You said: ");
 //        Serial.println(clf.predictLabel(features));
         float prediction[NUMBER_OF_LABELS];
+        float result = 0;
         tf_model.predict(features, prediction);
         for (int i = 0; i < NUMBER_OF_LABELS; i++) {
           Serial.print("Label ");
           Serial.print(words[i]);
           Serial.print(" = ");
           Serial.println(prediction[i]);
+          if (prediction[i] > 0.5){
+            switchCharacteristic.writeValue(sends[i]);
+          }
         }
         delay(1000);
     }
